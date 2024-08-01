6e930ed868964ec359151ef3762aed3c66af8c15
Romain Barbero <barsat1004@gmail.com>
2024-08-01 18:09:03 +0200

    feat: add banner

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-const body = document.querySelector('body')
-body.innerHTML = `
- <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-`
+function displayBanner() {
+  const app = document.getElementById("app");
+  const banner = document.createElement("div");
+
+  banner.className =
+    "bg-[url(./public/banner.png)] w-full h-[667px] bg-no-repeat bg-cover flex justify-center items-center";
+
+  banner.innerHTML = `
+  <img src="./public/logo.svg" alt="logo" class="w-[207px] absolute top-12 left-16">
+  <div class="max-w-[66.667%] text-center flex justify-center items-center">
+    <h2 class="text-yellow text-[44px] font-anton max-w-2xl">CHERCHEZ PARMI PLUS DE 1500 RECETTES DU QUOTIDIEN,SIMPLES ET DÉLICIEUSES</h2>
+  </div>
+  `;
+
+  app.appendChild(banner);
+}
+
+function init() {
+  try {
+    displayBanner();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+init();
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 export default {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: ["12px", "auto"],
+      base: ["14px", "auto"],
+      lg: ["16px", "auto"],
+      xl: ["18px", "auto"],
+    },
+    fontFamily: {
+      anton: ["Anton", "system-ui", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        yellow: "#FFD15B",
+        black: "#1B1B1B",
+        grey: "#7A7A7A",
+        "light-grey": "#C6C6C6",
+      },
+    },
   },
   plugins: [],
-}
-
+};
